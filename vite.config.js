ca18ed8ace9212c60d07b51cf9025c75cc3d7472
Bogdoing <Bogdoingg@gmail.com>
2025-06-07 23:55:19 +0300

    dal dev mode

--- vite.config.js
+++ vite.config.js
@@ -10,9 +10,9 @@ export default defineConfig({
     tailwindcss(),
     VitePWA({
       registerType: 'autoUpdate',
-      devOptions: {
-        enabled: true
-      }
+      // devOptions: {
+      //   enabled: true
+      // }
     })
   ],
 })
