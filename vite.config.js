cca0b5fa5974a7e4acf6cc3165bd6b8e6d927bf2
Bogdoing <Bogdoingg@gmail.com>
2025-06-17 21:04:49 +0300

    add sw config

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,27 @@ export default defineConfig({
     vue(),
     tailwindcss(),
     VitePWA({
-      registerType: 'autoUpdate'
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      includeAssets: ['logo.svg'],
+      manifest: {
+        name: 'My Awesome App',
+        short_name: 'MyApp',
+        description: 'My Awesome App description',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'logo.svg',
+            sizes: '192x192',
+            type: 'image/svg'
+          },
+          {
+            src: 'logo.svg',
+            sizes: '512x512',
+            type: 'image/svg'
+          }
+        ]
+      }
       // devOptions: {
       //   enabled: false
       // }
